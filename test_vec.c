42e6c9083dce98caefea5ad31b6ac3ae03f5e2f4
XunRong Tan <tanxrong@gmail.com>
2013-09-10 17:47:53 +0800

    CUINT

--- test_vec.c
+++ test_vec.c
@@ -2,6 +2,8 @@
 #include <string.h>
 
 #include "vector.h"
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 
 int main(int argc,char *argv[])
 {
@@ -14,20 +16,13 @@ int main(int argc,char *argv[])
     }
     vector_insert(tt,(void*)str2,strlen(str2)+1,14);
     vector_set(tt,(void*)str2,strlen(str2)+1,7);
-    printf("the 7th %s\n",(char*)vector_get(tt,7));
+    CU_ASSERT_STRING_EQUAL((char*)vector_get(tt,7),str2);
     vector_remove(tt,15);
-    for(int i = 0;i< tt->length;i++)
-    {
-        printf("%s\n",(char*)vector_get(tt,i));
-    }
+    CU_ASSERT_TRUE(tt->length == 15);
     vector_ptr subtt = vector_subvec(tt,7,14);
     vector_swap(subtt,0,1);
     int idx = vector_index(subtt,(void*)str2,strlen(str2)+1);
-    printf("idx %d \n",idx);
-    for(int i = 0;i< subtt->length;i++)
-    {
-        printf("%s\n",(char*)vector_get(subtt,i));
-    }
+    CU_ASSERT_EQUAL(idx,1);
     destroy_vector(tt);
     destroy_vector(subtt);
     return 0;
