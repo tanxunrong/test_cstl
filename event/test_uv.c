5942ab0330213f15bb687cbd5e38a0d721c942ea
XunRong Tan <tanxrong@gmail.com>
2014-08-20 18:55:49 +0800

    sock server test

--- event/test_uv.c
+++ event/test_uv.c
@@ -1,24 +1,81 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
 #include <uv.h>
 
-int64_t counter = 0;
+struct leela_conn {
+    int id;
+    int fd;
+    struct sockaddr_in addr;
+    uv_tcp_t userv;
+    uv_buf_t buff;
+};
 
-void wait_for_a_while(uv_idle_t* handle) {
-    counter++;
+struct leela_sock_server {
+//    int poll_fd;
+    int num;
+    uint session_id;
+    GMutex mutex;
+    GQueue *conn_list;
+    uv_tcp_t userv;
+};
 
-    if (counter >= 10e7)
-        uv_idle_stop(handle);
+static struct leela_sock_server *GS = NULL;
+
+void leela_on_read(uv_stream_t)
+void leela_on_conn(uv_stream_t *st,int status)
+{
+    if (status == -1)
+    {
+        return ;
+    }
+
+    struct leela_conn *conn = g_malloc0(sizeof(*conn));
+    g_mutex_lock(&GS->mutex);
+
+    g_queue_push_tail(GS->conn_list);
+    conn->id = GS->session_id + 100;
+    GS->session_id = (uint)(GS->session_id + 100);
+    GS->num++;
+
+    g_mutex_unlock(&GS->mutex);
+
+    uv_tcp_init(uv_default_loop(),conn->userv);
+
+    if (uv_accept(st,conn->userv) == 0)
+    {
+        uv_read_start(conn->userv,NULL,leela_on_read);
+    }
 }
 
 int main(int argc,char *argv[])
 {
-    uv_idle_t idler;
+    uv_loop_t *loop = uv_default_loop();
+    struct leela_sock_server *server = g_malloc0(sizeof(*server));
+    server->conn_list = g_queue_new();
+    g_mutex_init(&server->mutex);
+    uv_tcp_init(loop,&server->userv);
+    server->session_id = 10000;
+    GS = server;
 
-    uv_idle_init(uv_default_loop(), &idler);
-    uv_idle_start(&idler, wait_for_a_while);
+    struct sockaddr_in bind_addr ;
+    int ret = 0;
+    if ((ret = uv_ip4_addr("0.0.0.0",7890,&bind_addr)))
+    {
+        printf("addr wrong : %s\n",uv_err_name(ret));
+        exit(-1);
+    }
 
-    printf("Idling...\n");
-    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+    uv_tcp_bind(&server->userv,&bind_addr,0);
+
+    ret = uv_listen((uv_stream_t *)&server->userv,2<<8,leela_on_conn);
+    if (ret)
+    {
+        printf("listen err %s\n",uv_err_name(ret));
+        exit(-1);
+    }
+
+    uv_run(loop,UV_RUN_DEFAULT);
     return 0;
 }
